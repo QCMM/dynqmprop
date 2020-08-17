0198e99d5e849bb2bed34625591ea8fbdc8f7ced
Luis Macaya <lmacaya@users.noreply.github.com>
2020-08-17 11:30:39 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from .dynqmprop import DynQMProp
+#from .ffparams import ForceFieldParams
+#from .mdtools import (set_restrained_atoms, setup_simulation, calculate_charges,
+#                      charge_stats, epol_stats, make_new_top)
