46a7e0bdd782a036989a58e8bf71ac56c72becab
Luis Macaya <lmacaya@users.noreply.github.com>
2021-06-17 19:20:27 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-from .dynqmprop import DynQMProp
-#from .ffparams import ForceFieldParams
-#from .mdtools import (set_restrained_atoms, setup_simulation, calculate_charges,
-#                      charge_stats, epol_stats, make_new_top)
